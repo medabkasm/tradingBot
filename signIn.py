3cdd6700e2674823b67d4c955b51881626693e7f
medabkasm <51716754+medabkasm@users.noreply.github.com>
2019-07-05 14:59:07 +0100

    Update signIn.py
    changing signIn strategy

--- signIn.py
+++ signIn.py
@@ -1,21 +1,59 @@
-def sign_in():
-    try:
-        apiFile = open('api.txt','r')
-        apiKey = apiFile.readline(1)
-        secretKey = apiFile.readline(2)
-    except:
-        print('Error while reading the api file ')
-        return False
+from binance.client import Client
+import binance.exceptions as expt
 
-    try:
-        client = Client(apiKey,secretKey)
-    except:
-        print('Error : can''t sign in ')
-        return False
+
+def sign_in(apiFile = False):
+
+    if apiFile:
+        clientStatus = False
+        try:
+            with open("apiKey.txt","r") as apiFile :
+                (apiKeyText,apiKey) = apiFile.readline().split(":",1)
+                (secretKeyText,secretKey) = apiFile.readline().split(":",1)
+                print(apiKeyText)
+                print(apiKey)
+                print(secretKeyText)
+                print(secretKey)
+        except IOError as ioError:
+            print('Error :: File : {} .'.format(str(ioError)))
+            return False
+
+        if apiKeyText == "API_KEY" and secretKey == "SECRET_KEY" :
+            try:
+                client = Client(apiKey.rstrip(),secretKey.rstrip())
+                clientStatus = True
+
+            except:
+                print(' Error : Account can''t be opened correctelly .')
+                pass
+        else:
+            print('Error : File structure is not correct .')
+            print('You need to enter your api key and secret key manually .')
+            apiKey = input('API KEY : ')
+            secretKey = input('SECRET KEY : ')
+            client = Client(apiKey.rstrip(),secretKey.rstrip())
+
+    else:
+        apiKey = input('API KEY : ')
+        secretKey = input('SECRET KEY : ')
+        try:
+            client = Client(apiKey.rstrip(),secretKey.rstrip())
+            clientStatus = True
+
+        except:
+            print(' Error :: Account can''t be opened be opened correctelly . ')
+            pass
 
     status = client.get_system_status()
-    if(status['status']):
-        print(status['msg'])
+    if(status['status'] == 1 ):
+        print('Error :: System Status : '+ status['msg'])
         return False
     else:
+        try:
+            state = client.get_account()
+        except expt.BinanceAPIException as err:
+            print('Error :: '+str(err) + '.')
+        except expt.BinanceRequestException as err:
+            print('Error :: '+str(err) + '.')
+
         return client
